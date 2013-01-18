5d7f693a0b41fcc07e5e945fb027e09c6ca68219
Robert Monahon <bobby.monahon@gmail.com>
2013-01-17 22:47:52 -0800

    Initial Commit

---
+++ MagicLife1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MagicLife1
+//
+//  Created by Robert Monahon on 1/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ MagicLife1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MagicLife1
+//
+//  Created by Robert Monahon on 1/17/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
